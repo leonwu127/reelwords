2b39e46cb4110f1fe776f66b8bc990242a801df3
leonwu127 <leonwu127@gmail.com>
2023-10-11 19:50:26 +0100

    add tests

---
+++ ReelWords/GameLogic/UI/ConsoleUserInterface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReelWords.GameLogic.UI
+{
+    public class ConsoleUserInteraction : IUserInteraction
+    {
+        public string GetPlayerInput()
+        {
+            Console.Write("Enter a word: ");
+            return Console.ReadLine();
+        }
+
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string Prompt(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ ReelWords/GameLogic/UI/IUserInteraction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReelWords.GameLogic.UI
+{
+    public interface IUserInteraction
+    {
+        string GetPlayerInput();
+        void DisplayMessage(string message);
+        string Prompt(string message);
+    }
+}
